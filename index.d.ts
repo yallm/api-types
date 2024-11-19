3b5c88efe58716f4fae7cc1d3a025e620a13a2d0
Matte <51192395+MatteZ02@users.noreply.github.com>
2024-11-19 12:05:43 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,23 @@
 declare module "microtus-api-types" {
   export interface Payload {
-    event: "prompt" | "message" | "error";
+    event: "prompt" | "message" | "error" | "stop";
+    id: number;
     data: any;
   }
 
   export interface PromptPayload extends Payload {
     event: "prompt";
+    id: number;
     data: {
       query: string;
-      id: number;
       model: string;
       chat_history: string[];
     };
+  }
+
+  export interface StopPayload extends Payload {
+    event: "stop";
+    id: number;
   }
 
   export interface fileInfo {
